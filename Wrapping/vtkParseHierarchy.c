d4b405002527e4a67b002b39a01fabf86f535674
David Gobbi <david.gobbi@gmail.com>
2010-07-03 19:27:54 -0600

    BUG: skip_name must also skip over template args

--- Wrapping/vtkParseHierarchy.c
+++ Wrapping/vtkParseHierarchy.c
@@ -18,24 +18,38 @@
 #include <stdlib.h>
 #include <ctype.h>
 
-static int skip_space(const char *text)
+static size_t skip_space(const char *text)
 {
-  int i = 0;
+  size_t i = 0;
   while (isspace(text[i]) && text[i] != '\n') { i++; }
   return i;
 }
 
-static int skip_name(const char *text)
+static size_t skip_name(const char *text)
 {
-  int i = 0;
-  if (isalpha(text[i]) || text[i] == '_')
+  unsigned int depth = 0;
+  size_t i = 0;
+
+  if (isalpha(text[i]) || text[i] == '_' ||
+      (text[i] == ':' && text[i+1] == ':'))
     {
+    if (text[i] == ':') { i++; }
     i++;
     while (isalnum(text[i]) || text[i] == '_' ||
-           (text[i] == ':' && text[i+1] == ':'))
+           (text[i] == ':' && text[i+1] == ':') ||
+           text[i] == '<' || text[i] == '>')
       {
-      i++;
+      if (text[i] == '<')
+        {
+        while (text[i] != '\0' && text[i] != '\n')
+          {
+          if (text[i] == '<') { depth++; }
+          if (text[i] == '>') { if (--depth == 0) { break; } }
+          i++;
+          }
+        }
       if (text[i] == ':') { i++; }
+      i++;
       }
     }
   return i;
@@ -49,7 +63,7 @@ HierarchyInfo *vtkParseHierarchy_ReadFile(const char *filename)
   int maxClasses = 512;
   FILE *fp;
   char line[2048];
-  int i, j, n;
+  size_t i, j, n;
 
   fp = fopen(filename, "r");
 
