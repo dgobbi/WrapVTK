e9b292b1adb09de01c10eb31ee846770203f8735
David Gobbi <david.gobbi@gmail.com>
2012-10-26 18:39:27 -0600

    ENH: Make WrapVTK work with VTK version 6 (still experimental)

--- CMake/vtkIncludeAllKits.cmake
+++ CMake/vtkIncludeAllKits.cmake
@@ -9,46 +9,72 @@
 # VTK_COMMON_WRAP_HEADERS (list of headers to wrap)
 
 # Find all the kits that are part of the VTK build
-FILE(GLOB KIT_CMAKE_FILES RELATIVE "${VTK_KITS_DIR}"
-     "${VTK_KITS_DIR}/vtk*Kit.cmake")
+IF(VTK_MODULES_DIR)
 
-#MESSAGE("${VTK_KITS_DIR}")
-#MESSAGE("${KIT_CMAKE_FILES}")
-#MESSAGE("${VTK_KITS}")
+  #include("${VTK_DIR}/VTKTargets.cmake")
 
-SET(KITS_EXCLUDE)
-SET(TMP_VTK_KITS ${VTK_KITS})
-SET(VTK_KITS)
+  # use the old "Kit" variable names
+  set(VTK_KITS)
 
-FOREACH(VTK_KIT ${TMP_VTK_KITS})
+  foreach(mod ${VTK_MODULES_ENABLED})
+    include("${VTK_MODULES_DIR}/${mod}.cmake")
+    if(NOT ${mod}_EXCLUDE_FROM_WRAPPING)
+      include("${VTK_MODULES_DIR}/${mod}-Classes.cmake")
 
-  SET(KIT_NAME)
+      string(REGEX REPLACE "vtk(.*)" "\\1" TMP_NAME "${mod}")
+      string(TOUPPER "${TMP_NAME}" KIT_NAME)
 
-  # Match VTK_KIT to a file in KIT_CMAKE_FILES by doing an uppercase
-  # compare with underscores removed.
-  STRING(REPLACE "_" "" KIT_NO_UNDERSCORE ${VTK_KIT})
-  FOREACH(TMP_FILE ${KIT_CMAKE_FILES})
-    STRING(REGEX REPLACE "vtk(.*)Kit.cmake" "\\1" TMP_NAME "${TMP_FILE}")
-    STRING(TOUPPER "${TMP_NAME}" TMP_NAME_UPPERCASE)
-    IF("${TMP_NAME_UPPERCASE}" STREQUAL "${KIT_NO_UNDERSCORE}")
-      SET(KIT_NAME "${TMP_NAME}")
-      SET(KIT_CMAKE_FILE "${TMP_FILE}")
-    ENDIF("${TMP_NAME_UPPERCASE}" STREQUAL "${KIT_NO_UNDERSCORE}")
-  ENDFOREACH(TMP_FILE ${KIT_CMAKE_FILES})
+      set(VTK_${KIT_NAME}_NAME ${mod})
+      set(VTK_${KIT_NAME}_HEADER_DIR ${${mod}_INCLUDE_DIRS})
+      set(VTK_${KIT_NAME}_CLASSES ${${mod}_CLASSES})
+      set(VTK_${KIT_NAME}_WRAP_HEADERS)
 
-  # In the above FOREACH, the variables KIT_NAME and KIT_CMAKE_FILE were set
-  #MESSAGE("${VTK_KIT} ${KIT_NAME} ${KIT_CMAKE_FILE}")
+      list(APPEND VTK_KITS ${KIT_NAME})
+    endif()
+  endforeach()
 
-  # Get important variables from the kit's cmake file
-  IF(KIT_NAME)
-    INCLUDE("${VTK_KITS_DIR}/${KIT_CMAKE_FILE}")
-    SET(VTK_KITS ${VTK_KITS} ${VTK_KIT})
-  ELSE(KIT_NAME)
-    SET(KITS_EXCLUDE ${KITS_EXCLUDE} ${KIT_NAME})
-  ENDIF(KIT_NAME)
+ELSE(VTK_MODULES_DIR)
+  FILE(GLOB KIT_CMAKE_FILES RELATIVE "${VTK_KITS_DIR}"
+       "${VTK_KITS_DIR}/vtk*Kit.cmake")
 
-  # Set the "proper" name for the kit
-  SET(VTK_${VTK_KIT}_NAME "${KIT_NAME}")
+  #MESSAGE("${VTK_KITS_DIR}")
+  #MESSAGE("${KIT_CMAKE_FILES}")
+  #MESSAGE("${VTK_KITS}")
 
-ENDFOREACH(VTK_KIT ${TMP_VTK_KITS})
+  SET(KITS_EXCLUDE)
+  SET(TMP_VTK_KITS ${VTK_KITS})
+  SET(VTK_KITS)
 
+  FOREACH(VTK_KIT ${TMP_VTK_KITS})
+
+    SET(KIT_NAME)
+
+    # Match VTK_KIT to a file in KIT_CMAKE_FILES by doing an uppercase
+    # compare with underscores removed.
+    STRING(REPLACE "_" "" KIT_NO_UNDERSCORE ${VTK_KIT})
+    FOREACH(TMP_FILE ${KIT_CMAKE_FILES})
+      STRING(REGEX REPLACE "vtk(.*)Kit.cmake" "\\1" TMP_NAME "${TMP_FILE}")
+      STRING(TOUPPER "${TMP_NAME}" TMP_NAME_UPPERCASE)
+      IF("${TMP_NAME_UPPERCASE}" STREQUAL "${KIT_NO_UNDERSCORE}")
+        SET(KIT_NAME "${TMP_NAME}")
+        SET(KIT_CMAKE_FILE "${TMP_FILE}")
+      ENDIF("${TMP_NAME_UPPERCASE}" STREQUAL "${KIT_NO_UNDERSCORE}")
+    ENDFOREACH(TMP_FILE ${KIT_CMAKE_FILES})
+
+    # In the above FOREACH, the variables KIT_NAME and KIT_CMAKE_FILE were set
+    #MESSAGE("${VTK_KIT} ${KIT_NAME} ${KIT_CMAKE_FILE}")
+
+    # Get important variables from the kit's cmake file
+    IF(KIT_NAME)
+      INCLUDE("${VTK_KITS_DIR}/${KIT_CMAKE_FILE}")
+      SET(VTK_KITS ${VTK_KITS} ${VTK_KIT})
+    ELSE(KIT_NAME)
+      SET(KITS_EXCLUDE ${KITS_EXCLUDE} ${KIT_NAME})
+    ENDIF(KIT_NAME)
+
+    # Set the "proper" name for the kit
+    SET(VTK_${VTK_KIT}_NAME "${KIT_NAME}")
+
+  ENDFOREACH(VTK_KIT ${TMP_VTK_KITS})
+
+ENDIF(VTK_MODULES_DIR)
