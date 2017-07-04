06dd87bad99313bbd14263bd41f0187965ca0a1c
David Gobbi <david.gobbi@gmail.com>
2017-07-04 16:29:07 -0600

    Add ZEROCOPY hint attribute.

--- Wrapping/vtkParse.tab.c
+++ Wrapping/vtkParse.tab.c
@@ -12183,6 +12183,11 @@ void handle_attribute(const char *att, int pack)
       {
       setTypeMod(VTK_PARSE_NEWINSTANCE);
       }
+    else if (strcmp(att, "vtk::zerocopy") == 0 &&
+             role == VTK_PARSE_ATTRIB_DECL)
+      {
+      setTypeMod(VTK_PARSE_ZEROCOPY);
+      }
     else
       {
       print_parser_error("attribute cannot be used here", att, l);
